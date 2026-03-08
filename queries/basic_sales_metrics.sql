c37997bcf0cd11c3cc3fd189bd80a6c4d451ea71
Atip-Infa <theatip99@gmail.com>
2026-03-08 09:49:17 +0700

    Update basic_sales_metrics.sql

--- queries/basic_sales_metrics.sql
+++ queries/basic_sales_metrics.sql
@@ -1,1 +1,23 @@
+-- =====================================================
+--- BASIC SALES METRICES
+-- =====================================================
 
+--- TOTAL REVENUE
+SELECT SUM(sales_amount) AS total_revenue
+FROM gold.fact_sales;
+
+--- TOTAL NUMBER OF ORDERS
+SELECT COUNT(DISTINCT order_number) AS total_orders
+FROM gold.fact_sales;
+
+--- TOTAL QUANTITY SOLD
+SELECT SUM(quantity) AS total_units_sold
+FROM gold.fact_sales;
+
+--- AVERAGE ORDER VALUE
+SELECT AVG(sales_amount) AS average_order_value
+FROM gold.fact_sales;
+
+--- AVERAGE PRODUCT PRICE
+SELECT AVG(price) AS average_price
+FROM gold.fact_sales;
