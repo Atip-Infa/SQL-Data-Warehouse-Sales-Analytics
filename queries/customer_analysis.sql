f6c8d79d56ced079d6b0b3fe6e4e6a792c283d8a
Atip-Infa <theatip99@gmail.com>
2026-03-08 10:10:07 +0700

    Update customer_analysis.sql

--- queries/customer_analysis.sql
+++ queries/customer_analysis.sql
@@ -1,1 +1,51 @@
+-- =====================================================
+--- CUSTOMER ANALYSIS
+-- =====================================================
 
+--- REVENUE BY COUNTRY
+SELECT
+ c.customer_country,
+ SUM(f.sales_amount) AS revenue
+FROM gold.fact_sales f
+JOIN gold.dim_customers c
+ON f.customer_key = c.customer_key
+GROUP BY c.customer_country
+ORDER BY revenue DESC
+
+--- TOTAL CUSTOMERS
+SELECT COUNT(*) AS total_customers
+FROM gold.dim_customers
+
+--- REVENUE BY GENDER
+SELECT
+ c.customer_gender,
+ SUM(f.sales_amount) AS revenue
+FROM gold.fact_sales f
+JOIN gold.dim_customers c
+ON f.customer_key = c.customer_key
+GROUP BY c.customer_gender
+
+--- TOP 10 CUSTOMERS BY REVENUE
+SELECT TOP 10
+ c.customer_id,
+ c.customer_first_name,
+ c.customer_last_name,
+SUM(f.sales_amount) AS revenue
+FROM gold.fact_sales f
+JOIN gold.dim_customers c
+ON f.customer_key = c.customer_key
+GROUP BY
+ c.customer_id,
+ c.customer_first_name,
+ c.customer_last_name
+ORDER BY revenue DESC
+
+--- ORDERS PER CUSTOMER
+SELECT
+ c.customer_id,
+COUNT(DISTINCT f.order_number) AS total_orders
+FROM gold.fact_sales f
+JOIN gold.dim_customers c
+ON f.customer_key = c.customer_key
+GROUP BY c.customer_id
+ORDER BY total_orders DESC
